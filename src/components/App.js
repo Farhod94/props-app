524c631b97c5a7f35cf815c2eb151e1ac842f9a3
axmatovfarhod <axmatov.farxod.94@mail.ru>
2023-07-16 08:31:30 +0500

    App.js and Card.js update

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,32 @@
 import React from "react";
 import Card from "./Card";
+import Movies from "./movies";
 const App = () =>{
     return(
-        <Card />
+        <div>
+            <h1 className="heading">My movies</h1>
+        <Card
+            name = {Movies[0].name}
+            src = {Movies[0].imgUrl}
+            price = {Movies[0].price}
+            year = {Movies[0].year}
+            title = "Titanic"
+        />,
+        <Card 
+            name = {Movies[1].name}
+            src = {Movies[1].imgUrl}
+            price = {Movies[1].price}
+            year = {Movies[1].year}
+            title = "X-Man"
+        />,
+        <Card 
+        name = {Movies[2].name}
+        src = {Movies[2].imgUrl}
+        price = {Movies[2].price}
+        year = {Movies[2].year}
+        title = "Avatar"
+    />
+    </div>
     );
 };
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Movies from "./movies";
-import ReactDom from "react-dom";
+
 const Card = (props) =>{
     return(
         <div>
-            <h1 className="heading">My movies</h1>
+            
             <div className="Card">
                 <h1>{props.name}</h1>
                 <img src={props.src} alt={props.title} />
@@ -14,32 +13,5 @@ const Card = (props) =>{
         </div>
     );
 };
-
-ReactDom.render(
-    <div>
-    <Card 
-    name = {Movies[0].name}
-    src = {Movies[0].imgUrl}
-    price = {Movies[0].price}
-    year = {Movies[0].year}
-    title = "Titanic"
-    />
-    <Card 
-    name = {Movies[1].name}
-    src = {Movies[1].imgUrl}
-    price = {Movies[1].price}
-    year = {Movies[1].year}
-    title = "X-Man"
-    />
-    <Card 
-    name = {Movies[2].name}
-    src = {Movies[2].imgUrl}
-    price = {Movies[2].price}
-    year = {Movies[2].year}
-    title = "Avatar"
-    />
-    </div>,
-    document.getElementById("root")
-);
 
 export default Card;
